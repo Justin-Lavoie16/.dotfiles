ab6f33307b265e5bee70ecb6acc1e6101513aedf
Justin Lavoie <113055699+Justin-Lavoie16@users.noreply.github.com>
2024-10-03 09:25:50 -0400

    fixes bug

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -3,6 +3,13 @@
 
 return {
   {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     config = function()
